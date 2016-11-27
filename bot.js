240217c0b59b8142abd571d78d50fea62650a942
Dmitry Akentyev <TogusaRusso@gmail.com>
2016-11-27 13:32:48 +0000

    Checks for old urls using MongoDB

--- bot.js
+++ bot.js
@@ -2,10 +2,39 @@
 
 const Telegram = require('node-telegram-bot-api')
 const parser = require('./parser')
+const MongoClient = require('mongodb').MongoClient
 
-const tg = new Telegram(process.env.TOKEN)
+const dbUri = process.env.MONGOLAB_URI ||
+  process.env.MONGODB_URI ||
+  'mongodb://localhost:27017'
 
-parser((url) => {
-  console.log(`Sending ${url} to @${process.env.CHANNEL}`)
-  tg.sendPhoto(`@${process.env.CHANNEL}`, url)
+MongoClient.connect(dbUri, { server: { auto_reconnect: true } }, (err, db) => {
+  if (err) { return console.err(err) }
+  console.log('Connected correctly to mongodb')
+
+  const tg = new Telegram(process.env.TOKEN)
+  const collection = db.collection('urls')
+
+  parser((url) => {
+    console.log(`url=${url}`)
+    if (url) {
+      collection.find({url: url}).toArray((err, urls) => {
+        if (err) { return console.error(err) }
+        if (urls.length === 0) {
+          console.log(`that's new page`)
+          console.log(`Sending ${url} to @${process.env.CHANNEL}`)
+          tg.sendPhoto(`@${process.env.CHANNEL}`, url)
+          collection.insertOne({url: url}, (err, result) => {
+            if (err) { return console.error(err) }
+            console.log('Saved new url in database')
+            db.close()
+          })
+        } else {
+          console.log('nothing new here')
+          db.close()
+        }
+      })
+    }
+  })
 })
+
